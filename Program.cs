2188f4e88c6cdccfe3f3f5217b064f54aef0d930
Swapnil Mishra <63197684+swap72@users.noreply.github.com>
2025-05-18 15:21:29 +0530

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program{
+    static void Main(String[] args){
+        Console.WriteLine("Enter You Name: ");
+        String s=Console.ReadLine();
+        Console.WriteLine($"Welcome to the C# Console Application {s}");        
+    }
+}
